db7f9ec73ff86941fdc479a261dc13cd0ddbfb24
Gongyugyeong <girl93917@gmail.com>
2020-11-23 14:36:14 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
+/* eslint-disable */
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [title, setTitle] = useState(['title01', 'title02', 'title03']);
+  let [titleNumber, setTitleNumber] = useState(0);
+  let [like, setLike] = useState([0,1,2]);
+  let [modal, setModal] = useState(false);
+  let [input, setInput] = useState('');
+
+  let style = ({'cursor':'pointer'});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="black-nav">
+        <h1>blogTest</h1>
+      </div>
+      {title.map(function (name, index) {
+        return (
+          <div
+            className="list"
+            key={index}
+            onClick={() => {
+              setTitleNumber(index), setModal(true);
+            }}
+          >
+            <h3 className="titleName">
+              {name}{' '}
+              <span style={style} data-item={index}
+                onClick={(e) => {
+                  let data = e.target.dataset['item'];
+                  let likeCopy = [...like];
+                  
+                  data == index
+                  ? likeCopy[index] = likeCopy[index] + 1
+                  : null
+
+                  // data == 1
+                  // ? likeCopy[1] = likeCopy[1] + 1
+                  // : null
+
+                  // data == 2
+                  // ? likeCopy[2] = likeCopy[2] + 1
+                  // : null
+
+                  setLike(likeCopy);
+                }}
+              >
+                👍{' '}
+              </span>
+              {like[index]}
+            </h3>
+            <p>2020년 11월 0{index + 1}일</p>
+            <hr />
+          </div>
+        );
+      })}
+
+      <div className="publish">
+        <input
+          type="text"
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            let titleCopy = [...title];
+            titleCopy.unshift(input);
+            setTitle(titleCopy);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          저장
+        </button>
+      </div>
+
+      {/* <input type="text" onChange={(e)=>{ setInput(e.target.value) }} /> */}
+      <button
+        onClick={() => {
+          setModal(!modal);
+        }}
+      >
+        열고닫기
+      </button>
+
+      {modal === true ? (
+        <Modal title={title} titleNumber={titleNumber}></Modal>
+      ) : null}
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h3>{props.title[props.titleNumber]}</h3>
+      <p>내용</p>
     </div>
   );
 }
